51be80c5665e2059188e8acba735bdf5f9942f1c
Edison Simondet <edison.simondet@gmail.com>
2021-12-29 19:54:46 -0600

    further enhancements to delete functionality

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Note Taker Starter Code
+# Notetaker App
+
+## Purpose
+A simple note taking app
+
+## Description
+This project uses Express.js to host a database containing notes. 
+
+## Website
+You can view the live deployment [HERE](https://expressjs-notetaker-app.herokuapp.com/)
+
+## Screenshot of landing page
+![Full page screenshot](public/assets/images/full-page-screenshot.png)
--- routes/apiRotes/index.js
+++ routes/apiRotes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { createNewNote, validateNote } = require('../../lib/notes');
+const { createNewNote, validateNote, deleteById } = require('../../lib/notes');
 const { notesArray } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
@@ -7,14 +7,18 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = notesArray.length.toString();
 
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted!');
     } else {
-        const note = createNewNote(req.body, notes);
+        const note = createNewNote(req.body, notesArray);
         res.json(note);
     }
+});
+
+router.delete('/notes/:id', (req, res) => {
+    deleteById(req.params.id, notesArray);
 });
 
 module.exports = router;
